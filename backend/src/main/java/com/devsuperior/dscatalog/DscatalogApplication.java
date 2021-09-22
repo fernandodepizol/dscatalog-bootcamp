28d03c0773d350b1779d54670640428224c5d5ee
Fernando Depizol <luisfernando.depizol@gmail.com>
2021-09-21 23:07:33 -0300

    CategoryResource

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -3,11 +3,21 @@ package com.devsuperior.dscatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.dscatalog.entities.Category;
+
 @SpringBootApplication
 public class DscatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+		
+		Category categoria = new Category();
+		categoria.setId(1L);
+		categoria.setName("teste");
+		
+		System.out.println(categoria.toString());
 	}
+	
+	
 
 }
